3f949b378af4dd1147215fe40214c443fd2fbdea
SandWal <creambanana01@gmail.com>
2026-06-10 20:58:10 +0700

    Add files via upload

---
+++ dasar.py
@@ -0,0 +1,25 @@
+
+
+Sarapan = '07.00'
+Lunch ='12.00'
+Olahraga = '16.00'
+Dinner = '19.00'
+Belajar ='20.00'
+Tidur = '22.00'
+
+Jam = input("Jam berapakah ini ?")
+
+if Jam==Sarapan:
+    print("Waktunya Sarapan")
+elif Jam ==Lunch:
+    print("Waktunya Makan Siang")
+elif Jam == Olahraga :
+    print("Olahraga, Jaga Kesehatan")
+elif Jam == Dinner :
+    print ("Waktunya makan malam")
+elif Jam == Tidur:
+    print("Good Nght, Have a good dream")
+else :
+    print("Get Back To Work !")
+
+
