336b969bee6379928d9132fcd5c287a993c051f1
SandWal <creambanana01@gmail.com>
2026-06-18 12:01:23 +0700

    Add files via upload

---
+++ MENU.py
@@ -0,0 +1,14 @@
+menu = {"Kacang Ijo": 4000, 
+        "Nasi Goreng" : 10000,
+        "Kopi": 5000,
+        "Bubur ": 8000,
+        "Air Putih": 3000,     
+        }
+keranjang = []
+price = 0
+
+for key, value in menu.items():
+    print(f"{key:12} : Rp {value}")
+
+
+
